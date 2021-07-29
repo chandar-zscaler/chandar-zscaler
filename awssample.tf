90b78990dce16995ff5c22d50b6cc201df07eaf4
chandar-zscaler <87645781+chandar-zscaler@users.noreply.github.com>
2021-07-29 22:39:05 +0530

    Update awssample.tf

--- awssample.tf
+++ awssample.tf
@@ -17,7 +17,6 @@ resource "aws_instance" "app_seraver" {
   associate_public_ip_address = true
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
-  monitoring = true
 
   tags = {
     Name = "ExampleAppServerInstance"
