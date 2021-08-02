9ece46172c52f0c427b2db40c2e99d8b5e90fab3
chandar-zscaler <chandar.zscaler@gmail.com>
2021-08-02 11:26:04 +0530

    Update aws2.tf

--- aws2.tf
+++ aws2.tf
@@ -17,6 +17,12 @@ resource "aws_instance" "app_seraver" {
   associate_public_ip_address = true
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  monitoring = true
+  vpc_security_group_ids= "group"
+
+  metadata_options {
+    http_tokens = "required"
+  }
 
   tags = {
     Name = "ExampleAppServerInstance"
