82bb5a6959e032d12976b834e3e04ab3a66d5e6f
chandar-zscaler <87645781+chandar-zscaler@users.noreply.github.com>
2021-09-24 22:26:33 +0530

    Update awssample.tf

--- awssample.tf
+++ awssample.tf
@@ -25,12 +25,3 @@ resource "aws_instance" "app_seraver" {
     Name = "ExampleAppServerInstance"
   }
 }
-
-resource "aws_lambda_function" "tfer--418408845322-8e2D-acme-082D-dev-8e2D-analysis" {
-  environment {
-    variables{
-      access_key = "AKIAIOSFODNN7EXAMPLE"
-      secret_key = "wJalrXUtnFEMI/K7MDENG/bPXRf1CYEXAMPLEKEY"
-    }
-  }
-}
