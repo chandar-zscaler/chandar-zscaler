f3e881821cc6964c0b5cc0ed607b168cb003ef78
chandar-zscaler <87645781+chandar-zscaler@users.noreply.github.com>
2021-11-23 20:04:50 +0530

    Create test2.tf

---
+++ test2.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-1"
+}
+
+
+resource "aws_instance" "app_seraver" {
+  #ts:skip=AWS.AI.LM.HIGH.0070 I am skipping it for fun
+  associate_public_ip_address = true
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  monitoring = false
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+resource "aws_lambda_function" "tfer--418408845322-8e2D-acme-082D-dev-8e2D-analysis" {
+  environment {
+    variables{
+      access_key = "AKIAIOSFODNN7EXAMPLE"
+      secret_key = "wJalrXUtnFEMI/K7MDENG/bPXRf1CYEXAMPLEKEY"
+    }
+  }
+}
