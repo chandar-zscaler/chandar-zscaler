801e6fdd97bff3640f5ba56ac0985bf1c6afe7b8
chandar-zscaler <87645781+chandar-zscaler@users.noreply.github.com>
2021-11-30 17:48:23 +0530

    Update main.tf

--- modules/test/main.tf
+++ modules/test/main.tf
@@ -1,4 +1,4 @@
-resource "aws_lambda_function" "tfer--418408845322-8e2D-acme-082D-dev-8e2D-analysis" {
+resource "aws_lambda_function" "tfer--a418408845322-8e2D-acme-082D-dev-8e2D-analysis" {
   environment {
     variables{
       access_key = "AKIAIOSFODNN7EXAMPLE"
