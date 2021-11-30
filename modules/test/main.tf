f0f4d5d0bb6a32c5da65ace3edd3e9b5181ac818
chandar-zscaler <87645781+chandar-zscaler@users.noreply.github.com>
2021-11-30 17:44:49 +0530

    Create main.tf

---
+++ modules/test/main.tf
@@ -0,0 +1,8 @@
+resource "aws_lambda_function" "tfer--418408845322-8e2D-acme-082D-dev-8e2D-analysis" {
+  environment {
+    variables{
+      access_key = "AKIAIOSFODNN7EXAMPLE"
+      secret_key = "wJalrXUtnFEMI/K7MDENG/bPXRf1CYEXAMPLEKEY"
+    }
+  }
+}
